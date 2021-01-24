b72724dfb59f22e89c36f872753c908bfd32ea62
Chance Croft <66639666+ChanceJCroft@users.noreply.github.com>
2021-01-24 15:13:40 -0800

    Add files via upload

---
+++ public/aboutUsScript.js
@@ -0,0 +1,19 @@
+function changeWriter() {
+    let elem = document.getElementById("aboutStaff");
+    elem.innerHTML = "This is about the writer.";
+}
+
+function changeIllus() {
+    let elem = document.getElementById("aboutStaff");
+    elem.innerHTML = "This is about the Illustrators.";
+}
+
+function changeEditor() {
+    let elem = document.getElementById("aboutStaff");
+    elem.innerHTML = "This is about the Editors.";
+}
+
+function changeOther() {
+    let elem = document.getElementById("aboutStaff");
+    elem.innerHTML = "This is about everybody else.";
+}
